1e01966588fa93ca27204c2f22a8df624f9c8144
nicoagustinn <nicoagustinn@tutanota.com>
2021-12-06 13:52:42 -0300

    Now it does what it's supposed to. Has verbosity.

--- src/ren.c
+++ src/ren.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
-  
-int ren(char oldName[128],char newName[128]){
-    // Path to old and new files
-    // Input old and new file name
-    printf("Enter old file path: ");
-    scanf("%s", oldName);
+//
+//	YES I WILL REMOVE THE VERBOSITY
+//
 
-    printf("Enter new file path: ");
-    scanf("%s", newName);
-    // rename old file with new name
+int ren(char* oldName,char* newName){
+	printf("\nOld: %s",oldName);
+	printf("\nNew: %s",newName);
     if (rename(oldName, newName) == 0)
-        printf("File renamed successfully.\n");
+        printf("\nFile renamed successfully!\n");
     else
     printf("Unable to rename files!\nWrong name?\nNot enough permissions?\n");
 
@@ -19,15 +16,15 @@ int ren(char oldName[128],char newName[128]){
 int main(int argc,char* argv[]){
     int counter;
     printf("%s",argv[0]);
-    if(argc==1)
-        printf("\nUsage: (usage goes here)");
-    if(argc==2){
+
+    if(argc==3){
         printf("\nNumber Of Arguments Passed: %d",argc);
         printf("\n----Following Are The Command Line Arguments Passed----");
         for(counter=0;counter<argc;counter++)
             printf("\nargv[%d]: %s",counter,argv[counter]);
-    }
-    if (argc>=3)
+        ren(argv[1],argv[2]);}
+    else
     	return 1;
+
     return 0;
 }
