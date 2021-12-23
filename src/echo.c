2bdd9ae1c8793a5a2c3895c4416b5f94d63c33e7
Nicolás Agustín <84692683+nicoagustinn@users.noreply.github.com>
2021-12-23 10:37:42 -0300

    removed bloat
    does the same thing without those couple of lines, even works better since it adds a new line if argv is empty

--- src/echo.c
+++ src/echo.c
@@ -5,26 +5,17 @@
 int main(int argc, char *argv[]) {
 	int nflag;
 
-	if (argc <= 1) {
-		(void)fputs("",stdout);	
-		return 0;
-		}
-
 	if (*++argv && !strcmp(*argv, "-n")) {
 		++argv;
 		nflag = 1;
 	}
 	else
 		nflag = 0;
-
 	while (*argv) {
 		(void)fputs(*argv, stdout);
 		if (*++argv)
 			putchar(' ');
 	}
-
-	if (!nflag)
-		putchar('\n');
-
+	if (!nflag){putchar('\n');}
 	return 0;
 }
